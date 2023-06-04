dc69b7246a4f619d06078b029963287b6728a013
Argemiro <argemirojunior123@gmail.com>
2023-06-04 13:46:12 -0300

    second

--- prisma/migrations/20230119145035_create_books/migration.sql
+++ prisma/migrations/20230119145035_create_books/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "books" (
     "id" TEXT NOT NULL,
-    "isbn" INTEGER NOT NULL,
+    "isbn" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "author" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" REAL NOT NULL,
-    "quantity" INTEGER NOT NULL,
+    "price" TEXT NOT NULL,
+    "quantity" TEXT NOT NULL,
 
     CONSTRAINT "books_pkey" PRIMARY KEY ("id")
 );
