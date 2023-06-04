cccc0823b9b32f9fc07c65c6b63c07334eb98368
Argemiro <argemirojunior123@gmail.com>
2023-06-04 10:55:09 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,29 @@
-import express from 'express'
-import { prismaClient } from './database'
+import express from 'express';
+import cors from 'cors';
+import { prismaClient } from './database';
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:4000'
+}));
 
-const port = process.env.PORT ?? 4000
+const port = process.env.PORT || 4000;
 
 app.get('/books', async (request, response) => {
-  const books = await prismaClient.book.findMany()
-  return response.json(books)
-})
+  const books = await prismaClient.book.findMany();
+  return response.json(books);
+});
 
 app.post('/books', async (request, response) => {
-  const { description, name } = request.body
+  const { description, name } = request.body;
   const book = await prismaClient.book.create({
     data: {
       description,
       name,
     },
-  })
-  return response.json(book)
-})
+  });
+  return response.json(book);
+});
 
-app.listen(port, () => console.log('Server is running on port ', port))
+app.listen(port, () => console.log('Server is running on port ', port));
